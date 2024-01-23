42c034003613fc08109d96ea72382b4b73e1c51e
Mikel Larrañaga <mikel.larranaga@ehu.eus>
2024-01-23 18:14:01 +0100

    Añadido el método 1

--- src/main/java/eus/ehu/eui/isoft/Main.java
+++ src/main/java/eus/ehu/eui/isoft/Main.java
@@ -2,6 +2,11 @@ package eus.ehu.eui.isoft;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+    }
+
+    public static void method1() {
+        System.out.println("Esto es un método");
     }
 }
